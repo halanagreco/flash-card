cc3dbc4a3558e6296ad144eee2d8fc6d07b856b8
halanagreco <159548750+halanagreco@users.noreply.github.com>
2024-09-12 12:24:24 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,22 @@
+criaCartao(
+    'MATEMATICA',
+    'QUANTO É 5X3-6?',
+    '9'
+)
+
+criaCartao(
+    'MATEMATICA',
+    'CALCULE RÁPIDO 5+5x5+5= ?',
+    '35'
+)
+
+criaCartao(
+    '4X2+6=',
+     '14',
+)
+
+criaCartao(
+    'MATEMATICA',
+    ' 9 X? = 27',
+    '18'
+)
