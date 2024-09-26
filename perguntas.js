8d06f44197f018a5d96cc83efc24ace17a4e6f2b
halanagreco <159548750+halanagreco@users.noreply.github.com>
2024-09-26 12:16:23 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,22 +1,155 @@
 criaCartao(
-    'MATEMATICA',
-    'QUANTO É 5X3-6?',
-    '9'
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
 )
 
 criaCartao(
-    'MATEMATICA',
-    'CALCULE RÁPIDO 5+5x5+5= ?',
-    '35'
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
 )
 
 criaCartao(
-    '4X2+6=',
-     '14',
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
 )
 
 criaCartao(
-    'MATEMATICA',
-    ' 9 X? = 27',
-    '18'
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
 )
+
+criaCartao(
+    'geografia',
+    'o que o sol é',
+    'uma estrela',
+)
+
+criaCartao(
+    'portuges',
+    'Na língua portuguesa, como chamado o aumentativo de "FOGO"?',
+    'fogão'
+)
+
+criaCartao(
+    
+)
+
+criaCartao(
+    'matematica',
+    'O que é um número irracional?',
+    'É um número que não pode ser expresso como fração exata, como √2 e pi.',
+    
+)
+
+criaCartao(
+    'biologia',
+    '*O que é epigenética?',
+     'Estudo de mudanças na expressão gênica que não envolvem alterações no DNA, influenciadas por fatores ambientais e de estilo de vida',
+
+)
+
+criaCartao(
+    'geografia',
+    'O que é a linha do tempo geológica da Terra?', 
+    'É a divisão da história da Terra em eras e períodos com base em grandes eventos geológicos e biológicos, como a extinção dos dinossauro',
+
+    
+)
+
+criaCartao(
+   'fisica' 
+  'Como a fusão nuclear ocorre no sol?',
+   'A fusão nuclear acontece quando átomos de hidrogênio colidem e se fundem para formar hélio, liberando enormes quantidades de energia',
+
+)
+
+criaCartao(
+    'O que são números complexos?',  
+    'Números que têm uma parte real e uma parte imaginária, usados para resolver equações que não têm solução nos números reais',
+ 
+    
+)
+
+criaCartao(
+
+)
+
+criaCartao(
+    
+)
+
+criaCartao(
+    
+)
+
+criaCartao(
+    
+)
+
+criaCartao(
+    
+)
+
+criaCartao(
+    
+)
+
+criaCartao(
+    
+)
+
+criaCartao(
+    
+)
+
+criaCartao(
+
+)
+
+criaCartao(
+    
+)
+
+criaCartao(
+    
+)
+
+criaCartao(
+    
+)
+
+criaCartao(
+    
+)
+
+criaCartao(
+    
+)
+
+criaCartao(
+    
+)
+
+criaCartao(
+    
+)
+criaCartao(
+    
+)
+
+criaCartao(
+    
+)
+
+criaCartao(
+    
+)
+
+    
+
+
+
