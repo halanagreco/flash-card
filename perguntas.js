85d1ed81aced3a1fd21e93b8b37e3ca140b5785d
halanagreco <159548750+halanagreco@users.noreply.github.com>
2024-10-03 12:24:54 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -35,121 +35,148 @@ criaCartao(
 )
 
 criaCartao(
-    
+    'Matemática',
+    'O que são números transcendentes?',
+   'Números que não são raízes de nenhuma equação polinomial de coeficientes inteiros, como o número pi e o número e.',
 )
 
 criaCartao(
     'matematica',
     'O que é um número irracional?',
     'É um número que não pode ser expresso como fração exata, como √2 e pi.',
-    
 )
 
 criaCartao(
     'biologia',
-    '*O que é epigenética?',
+    'O que é epigenética?',
      'Estudo de mudanças na expressão gênica que não envolvem alterações no DNA, influenciadas por fatores ambientais e de estilo de vida',
-
 )
 
 criaCartao(
     'geografia',
     'O que é a linha do tempo geológica da Terra?', 
-    'É a divisão da história da Terra em eras e períodos com base em grandes eventos geológicos e biológicos, como a extinção dos dinossauro',
-
-    
+    'É a divisão da história da Terra em eras e períodos com base em grandes eventos geológicos e biológicos, como a extinção dos dinossauro',   
 )
 
 criaCartao(
-   'fisica' 
+   'fisica',
   'Como a fusão nuclear ocorre no sol?',
    'A fusão nuclear acontece quando átomos de hidrogênio colidem e se fundem para formar hélio, liberando enormes quantidades de energia',
-
 )
 
 criaCartao(
+    'Matemática',
     'O que são números complexos?',  
     'Números que têm uma parte real e uma parte imaginária, usados para resolver equações que não têm solução nos números reais',
- 
-    
 )
 
 criaCartao(
-
-)
-
-criaCartao(
-    
-)
-
-criaCartao(
-    
-)
-
-criaCartao(
-    
-)
-
-criaCartao(
-    
-)
-
-criaCartao(
-    
-)
-
-criaCartao(
-    
-)
-
-criaCartao(
-    
-)
-
-criaCartao(
+    'História',
+    'Como o uso da pólvora mudou as guerras medievais?', 
+   'A pólvora revolucionou a guerra ao permitir o uso de armas de fogo e canhões, acabando com a era dos castelos impenetráveis.',
 
 )
 
 criaCartao(
-    
+    'História',
+    'Quem foram os samurais?',
+    'Guerreiros japoneses da elite militar feudal que seguiam um rígido código de honra chamado bushido.',
 )
 
 criaCartao(
-    
+    'História',
+    'Como o Canal de Suez impactou o comércio mundial?', 
+    'Inaugurado em 1869, o Canal de Suez reduziu drasticamente o tempo de viagem entre a Europa e a Ásia, revolucionando o comércio global.',
 )
 
 criaCartao(
-    
+    'História',
+    'O que foi o Império Bizantino?',
+     'Era a continuação do Império Romano no Oriente, com a capital em Constantinopla, e durou mil anos após a queda do Império Romano do Ocidente.',
 )
 
 criaCartao(
-    
+    'História',
+    'Como as Cruzadas mudaram a Europa medieval?',
+    'As Cruzadas estimularam o comércio, promoveram o contato entre culturas e influenciaram o desenvolvimento de novas tecnologias na Europa.',
 )
 
 criaCartao(
-    
+    'Geografia',
+    'O que são zonas mortas oceânicas?',
+    'Áreas do oceano com pouco ou nenhum oxigênio, onde a vida marinha não pode sobreviver. Elas são causadas pelo excesso de nutrientes, muitas vezes vindos da poluição agrícola.',
+
 )
 
 criaCartao(
-    
+    'Geografia',
+    'Por que o deserto do Saara está se expandindo?', 
+    'O Saara está crescendo devido à desertificação, provocada por mudanças climáticas e a atividade humana, como desmatamento e práticas agrícolas inadequadas.',
 )
 
 criaCartao(
-    
-)
-criaCartao(
-    
-)
-
-criaCartao(
-    
+    'Geografia',
+    'Como surgem as ilhas vulcânicas?', 
+    'São formadas por erupções vulcânicas que trazem material derretido à superfície, eventualmente acumulando-se até formar terra acima do nível do mar.',
 )
 
 criaCartao(
-    
+    'Geografia',
+    'O que são os "rios voadores" da Amazônia?',
+    'São massas de vapor de água que sobem da floresta Amazônica e se deslocam pelos ventos, fornecendo umidade a regiões distantes',
 )
 
-    
+criaCartao(
+    'Biologia',
+    'Como as plantas carnívoras obtêm nutrientes?', 
+    'Elas capturam e digerem pequenos insetos ou animais para compensar a falta de nutrientes no solo pobre em que vivem.',
+)
 
+criaCartao(
+    'Biologia',
+    'O que é CRISPR?',
+    'Técnica revolucionária de edição genética que permite cortar e modificar o DNA de forma precisa',
+)
 
+criaCartao(
+    'Biologia',
+    'Por que os flamingos são cor-de-rosa?',
+    'Eles adquirem a coloração rosa por causa dos pigmentos carotenoides encontrados nos crustáceos e algas que comem.',
+)
 
+criaCartao(
+    'Biologia',   
+    'Como funciona o cérebro durante o sono REM?',
+    'Durante o sono REM, o cérebro está altamente ativo, processando memórias e emoções, e é o estágio em que os sonhos mais vívidos ocorrem.',
+)
+
+criaCartao(
+    'Química',
+    'O que é o estado de Bose-Einstein?', 
+    'Um estado exótico da matéria que ocorre a temperaturas extremamente baixas, onde partículas agem como um único superátomo.',
+)
+
+criaCartao(
+    'Química',
+    'Como funcionam os supercondutores?',
+    'Materiais que, quando resfriados abaixo de uma certa temperatura, conduzem eletricidade sem resistência, o que pode revolucionar o transporte de energia.',
+
+)
+
+criaCartao(
+    'Química',
+    'O que é o fenômeno da fluorescência?',
+    'É a emissão de luz por uma substância que absorveu luz ou outra radiação eletromagnética, como ocorre em águas-vivas.',
+)
+criaCartao(
+    'Química',
+    'O que é alótropo?',
+    'São formas diferentes do mesmo elemento químico, como o carbono, que pode formar grafite ou diamante.',
+
+)
+
+criaCartao(
+    'Química',
+    'Como os ácidos e bases se neutralizam?',
+    'Um ácido libera íons H⁺, enquanto uma base libera íons OH⁻. Quando combinados, formam água e um sal, neutralizando-se mutuamente.',
+)
